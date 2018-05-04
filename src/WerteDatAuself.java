7bce65b2d34fa25da535e39a1c3a980dd3a6c2db
CryptKid <CryptKid@users.noreply.github.com>
2018-05-04 15:09:12 +0200

    Added comperator function

--- src/WerteDatAuself.java
+++ src/WerteDatAuself.java
@@ -1,6 +1,27 @@
 public class WerteDatAuself {
+    boolean wasihacked = false;
+    int[] asort;
+    int[] bsort;
+    sortmaschine_exe sortdafoo;
     public WerteDatAuself(boolean hacktmode) {
+        wasihacked = hacktmode;
+    }
 
+    public int richtige(int[] a, int[] b) {
+        int y = 0;
+        int puffi = 0;
+        sortdafoo = new sortmaschine_exe();
+        asort = sortdafoo.sortmyfoo(a);
+        bsort = sortdafoo.sortmyfoo(b);
+        for (int i = 0; i < asort.length; i++ ) {
+            puffi = asort[i];
+            for (int j = 0; j < bsort.length; j++) {
+                if (asort[i] == bsort[j]) {
+                    puffi++;
+                }
+            }
+        }
+        return puffi;
     }
 
 }
