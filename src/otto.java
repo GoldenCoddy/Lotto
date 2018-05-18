bc39dc97f57971f8345caddca9cdedc86277cfc7
CryptKid <CryptKid@users.noreply.github.com>
2018-05-18 15:01:44 +0200

    Added feature to remove duplicated numbers in ott.java

--- src/otto.java
+++ src/otto.java
@@ -4,6 +4,7 @@ public class otto {
     int tmax;
     int tmin;
     int tcount;
+    sortmaschine_exe sortmatschine = new sortmaschine_exe();
 
     public otto(int count, int max, int min) {
         wuerfelergebnisse = new int[count];
@@ -17,6 +18,18 @@ public class otto {
             wuerfelergebnisse[i] = ThreadLocalRandom.current().nextInt(tmin, tmax);
         }//Math.random();
             // wuerfelergebnisse[i] = Math.round(Math.random());
+        boolean sucessfull = false;
+        while (!sucessfull) {
+            wuerfelergebnisse = sortmatschine.sortmyfoo(wuerfelergebnisse);
+            sucessfull = true;
+            for (int i = 0; i < wuerfelergebnisse.length - 1; i++) {
+                if (wuerfelergebnisse[i] == wuerfelergebnisse[i + 1]) {
+                    //dublicate = true;
+                    sucessfull = false;
+                    wuerfelergebnisse[i] = ThreadLocalRandom.current().nextInt(tmin, tmax);
+                }
+            }
+        }
             return wuerfelergebnisse;
         }
     }
